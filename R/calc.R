7592c4f4d81cb39b05521f9ed42be0ec1df8c919
Adam Ryczkowski <adam@statystyka.net>
2019-03-15 15:35:44 +0100

    Fixes the actual model execution

--- R/calc.R
+++ R/calc.R
@@ -1,6 +1,21 @@
 
-calc_models<-function(model_names, ads, dv_name, iv_names, path_prefix='models/',  adaptive=NA, assume_calculated=FALSE) {
+calc_models<-function(model_names, ads, dv_name, iv_names, path_prefix='models/',  adaptive=NA, assume_calculated=FALSE, objective_function=NA) {
   library(caret)
+  if(is.na(objective_function)) {
+    if('factor' %in% class(ads[[dv_name]])) {
+      if(length(as.list(table(ads[[dv_name]])))==2) {
+#        browser()
+        objective_function<-'ROC'
+        pos_level=levels(ads[[dv_name]])[[2]]
+        newvar=ads[[dv_name]]==pos_level
+        ads[[dv_name]]<-newvar
+      } else {
+        objective_function<-'Accuracy'
+      }
+    } else {
+      objective_function<-'RMSE'
+    }
+  }
 
   do_model_inner<-function(dv_name, model_name, ads, tc) {
     plik_name<-paste0(path_prefix, 'model_', dv_name, '_', model_name, '.rds')
@@ -27,22 +42,23 @@ calc_models<-function(model_names, ads, dv_name, iv_names, path_prefix='models/'
       } else {
         cat(paste0("Opening ", plik_name, "...\n"))
         write.table(data.frame(), file=plik_name, col.names=FALSE)
+#        browser()
         return(tryCatch(
           {
             if(is.na(adaptive)) {
               msg<-paste0("Trying adaptive train of model ", model_name, "...")
               cat(paste0(msg, "\n"))
-              model<-caret::train(dv ~ ., data = ads, method = model_name,
+              model<-caret::train(dv ~ ., data = ads, method = model_name, metric = objective_function,
                                   trControl = tc_adaptive, tuneLength=15)
             } else if(adaptive) {
               msg<-paste0("Calculating adaptive train of model ", model_name, "..")
               cat(paste0(msg, "\n"))
-              model<-caret::train(dv ~ ., data = ads, method = model_name,
+              model<-caret::train(dv ~ ., data = ads, method = model_name, metric = objective_function,
                                   trControl = tc_adaptive, tuneLength=15)
             } else {
               msg<-paste0("Calculating non-adaptive train of model ", model_name, "...")
               cat(paste0(msg, "\n"))
-              model<-caret::train(dv ~ ., data = ads, method = model_name, trControl = tc)
+              model<-caret::train(dv ~ ., data = ads, method = model_name, trControl = tc, metric = objective_function)
             }
             saveRDS(model, plik_name)
             return(msg)
@@ -53,7 +69,7 @@ calc_models<-function(model_names, ads, dv_name, iv_names, path_prefix='models/'
               cat(paste0(msg, "\n"))
               return(tryCatch(
                 {
-                  model<-caret::train(dv ~ ., data = ads, method = model_name, trControl = tc, tuneLength=15)
+                  model<-caret::train(dv ~ ., data = ads, method = model_name, trControl = tc, tuneLength=15, metric = objective_function)
                   saveRDS(model, plik_name)
                   return(msg)
                 },
@@ -79,27 +95,170 @@ calc_models<-function(model_names, ads, dv_name, iv_names, path_prefix='models/'
   #   groupvar<-ads[[keep_nominal]]
   #   cvIndex<-caret::createMultiFolds(groupvar, k = 10, times=10)
   # } else {
-    cvIndex<-caret::createMultiFolds(ads$dv, times=10,  k = 10)
 #  }
   selFun<-function(x, metric,  maximize) caret::oneSE(x=x, metric = metric, num=10, maximize = maximize)
-  tc_adaptive <- caret::trainControl(index = cvIndex,
-                                     method = 'adaptive_cv',
-                                     adaptive = list(min = 5, alpha = 0.05,
-                                                     method = "gls", complete = TRUE),
-                                     search = "random",
-                                     selectionFunction = selFun
-  )
-  if(adaptive) {
-    tc <- caret::trainControl(index = cvIndex,
-                              method = 'cv',
-                              number = 10, repeats = 10)
+  if(objective_function=='ROC') {
+    tc_adaptive <- caret::trainControl(method = 'adaptive_cv',
+                                       adaptive = list(min = 5, alpha = 0.05,
+                                                       method = "BT", complete = TRUE),
+                                       search = "random",
+                                       classProbs=TRUE,
+                                       summaryFunction = caret::twoClassSummary,
+                                       selectionFunction = selFun)
+    tc <- caret::trainControl(method = 'cv',
+                              number = 10, repeats = 10,
+                              classProbs=TRUE,
+                              summaryFunction = caret::twoClassSummary,
+                              selectionFunction = selFun)
   } else {
+    browser()
+    cvIndex<-caret::createMultiFolds(ads$dv, times=10,  k = 10)
+    tc_adaptive <- caret::trainControl(index = cvIndex,
+                                method = 'adaptive_cv',
+                                adaptive = list(min = 5, alpha = 0.05,
+                                                method = "BT", complete = TRUE),
+                                search - "random",
+                                selectionFunction = selFun)
     tc <- caret::trainControl(index = cvIndex,
                               method = 'cv',
-                              number = 10)
+                              number = 10, repeats = 10,
+                              selectionFunction = selFun)
   }
+
   model_names<-model_names[sample(seq_along(model_names))]
   models=setNames(purrr::map(model_names, do_model_inner, ads=ads, tc=tc, dv_name=dv_name), model_names)
 
   list(ads=ads, models=models)
 }
+
+model_perfs<- function(ans) {
+  valid_models<-which(!purrr::map_lgl(ans$models, is.character))
+  models<-ans$models[valid_models]
+  model_names<-names(models)
+  ads<-ans$ads
+
+
+
+  a1<-purrr::map_dbl(models, function(x) {caret::getTrainPerf(x)$TrainRMSE} )
+  a3<-purrr::map_dbl(models, function(x) {caret::getTrainPerf(x)$TrainRsquared} )
+  a2<-purrr::map_dbl(models, function(x) {caret::getTrainPerf(x)$TrainMAE} )
+  a4<-purrr::map_dbl(models, function(x) {caret::getTrainPerf(x)$TrainAccuracy} )
+  a5<-purrr::map_dbl(models, function(x) {caret::getTrainPerf(x)$TrainKappa} )
+  n1<-purrr::map_chr(models, function(x) {x$modelInfo$label} )
+  b1<-purrr::map_dbl(models, function(x) {as.numeric(x$times$everything['elapsed'])} )
+  b2<-purrr::map_dbl(models, function(x) {as.numeric(x$times$everything['user.self'])} )
+  b3<-purrr::map_dbl(models, function(x) {as.numeric(x$times$everything['user.child'])} )
+  b4<-purrr::map_chr(models, function(x) {paste(x$modelInfo$tags, collapse = ', ')} )
+  b4_1<-purrr::map_lgl(models, function(x) {'Neural Network' %in% x$modelInfo$tags} )
+  b4_2<-purrr::map_lgl(models, function(x) {'Bagging' %in% x$modelInfo$tags} )
+  b4_3<-purrr::map_lgl(models, function(x) {'Random Forest' %in% x$modelInfo$tags} )
+  b4_4<-purrr::map_lgl(models, function(x) {'Linear Regression' %in% x$modelInfo$tags} )
+  b4_5<-purrr::map_lgl(models, function(x) {'Bayesian Model' %in% x$modelInfo$tags} )
+  b4_6<-purrr::map_lgl(models, function(x) {'Implicit Feature Selection' %in% x$modelInfo$tags} )
+  b4_7<-purrr::map_lgl(models, function(x) {'Boosting' %in% x$modelInfo$tags} )
+  b4_8<-purrr::map_chr(models, function(x) {x$modelType} )
+  b5<-purrr::map(models, function(x) {list(setdiff(x$modelInfo$tags,
+                                                   c('Bagging', 'Implicit Feature Selection', 'Boosting')))} )
+
+  b5_grp<-rep(NA, length(b5))
+
+  fn<-function(b5_grp, tag, tag_level){
+    lgls<-which(purrr::map_lgl(b5, function(x) tag %in% x[[1]]))
+
+    tmp<-lgls[which(!is.na(b5_grp[lgls]))]
+    if(length(tmp)>0) {
+      for(i in tmp) {
+        cat(paste0("Model ", n1[[i]], "(",model_names[[i]],") is also of type ", b5_grp[[i]], ".\n"))
+      }
+    }
+    cat(paste0(length(lgls), " ", tag, "s\n"))
+    b5_grp[lgls]<-tag_level
+    return(b5_grp)
+  }
+
+  b5_grp<-fn(b5_grp, 'Linear Regression', 1)
+  b5_grp<-fn(b5_grp, 'Relevance Vector Machines', 2)
+  b5_grp<-fn(b5_grp, 'Support Vector Machines', 2)
+  b5_grp<-fn(b5_grp, 'Gaussian Process',3)
+  b5_grp<-fn(b5_grp, 'Random Forest', 4)
+  b5_grp<-fn(b5_grp, 'Multivariate Adaptive Regression Splines', 5)
+  b5_grp<-fn(b5_grp, 'Tree-Based Model', 4)
+  b5_grp<-fn(b5_grp, 'Neural Network', 6)
+  b5_grp[is.na(b5_grp)]<-7
+
+
+
+  df<-dplyr::arrange(tibble(model=model_names, name=n1,  rmse=a1, rsq=a2, mae=a3, elapsed_time=b1, user_time=b2+b3,
+                            is_nn=b4_1, is_bagging=b4_2, is_boost=b4_7,
+                            is_rf=b4_3, is_lm=b4_4, is_bayes=b4_5, is_feature_sel=b4_6, modelType = b4_8, tags = b5,
+                            model_family=factor(b5_grp, levels=sort(unique(b5_grp)), labels=c('Linear Regression', 'Support Vector Machines',
+                                                                                              'Gaussian Process', 'Random Forest and trees',
+                                                                                              'Multivariate Adaptive Regression Splines',
+                                                                                              'Neural Network', 'Other'))), rmse)
+
+  sub_df <- df %>% filter(b5_grp != "Linear Regression")
+  models_to_get_metric<-c('glmnet', sub_df$model)
+  models_hard_to_compare<-c('lasso', 'pls', 'rpart', 'rpart2', 'simpls')
+  models_hard_to_compare<-c('bagEarth', 'blasso', 'earth', 'gcvEarth', 'kernelpls', 'lars2', 'lasso', 'pls', 'pcr', 'rpart', 'rpart2', 'simpls', 'spikeslab')
+  #models_hard_to_compare<-c('bagEarth', 'blasso', 'earth', 'gcvEarth', 'kernelpls', 'lars2', 'lasso', 'pls', 'rpart', 'rpart2', 'simpls', 'spikeslab')
+  models_to_get_metric<-setdiff(sample(df$model), models_hard_to_compare)
+
+  df_to_remove<- df %>% filter(model %in% models_hard_to_compare)
+
+  #  browser()
+  # res_big<-summary(resamples(models[
+  #   c('rpart', 'rpart2', 'blasso', 'kernelpls')]), metric='RMSE', decreasing=TRUE)
+  # c('gcvEarth', 'earth', 'rpart', 'rpart2', 'simpls', 'pls', 'lasso', 'blasso', 'kernelpls')
+  # res_big<-summary(resamples(models[models_hard_to_compare[1:4]]), metric='RMSE', decreasing=TRUE)
+
+
+  # old_timing<-0
+  # old_size<-0
+  # for(i in seq(2, length(models_to_get_metric))) {
+  #   subsecik<-models_to_get_metric[seq(1, i)]
+  #   cat(paste0("Trying adding ", length(subsecik), "th model ", subsecik[[i]], " of size ", utils:::format.object_size(object.size(models[[i]]), "auto"), "\n"))
+  #   ts<-system.time(res<-summary(resamples(models[subsecik]), metric='RMSE', decreasing=TRUE))
+  #   new_timing<-ts[['elapsed']]
+  #   new_size<-object.size(res)
+  #   cat(paste0("Elapsed time: ", round(new_timing), " which is ", round(100*(new_timing-old_timing)/(length(subsecik)-1))/100, " sec per sample.\n"))
+  #   cat(paste0("Extra object size time: ", gdata::humanReadable(new_size - old_size),
+  #              " which is ", gdata::humanReadable(round((new_size-old_size)/(length(subsecik)-1))), " per sample.\n\n"))
+  #   old_timing<-new_timing
+  #   old_size<-new_size
+  # }
+
+
+  res<-summary(resamples(models[models_to_get_metric]), metric='RMSE', decreasing=TRUE)
+
+
+
+  #  res<-summary(resamples(models[df$model]), metric='RMSE', decreasing=TRUE)
+  rmse_3rd<-min(res$statistics$RMSE[,5]) #3rd quantile of the best model's RMSE
+  idx_ok<-rownames(res$statistics$RMSE)[(res$statistics$RMSE[,2]<rmse_3rd)] #Which models are not statistically worse then the best
+
+  # best_model<-df$model[[1]]
+  # mem_size<-0
+  # dftmp<-NULL
+  # for(i in seq(2, length(df$model))) {
+  #   ms<-models[c(best_model, df$model[[i]])]
+  #   res<-summary(resamples(ms), metric='RMSE', decreasing=TRUE)
+  #   if(is.null(dftmp)) {
+  #     dftmp<-res$statistics$RMSE
+  #   } else {
+  #     dftmp<-rbind(dftmp, res$statistics$RMSE[2,])
+  #     rownames(dftmp)<-c(rownames(dftmp)[seq(1, nrow(dftmp)-1)], rownames(res$statistics$RMSE)[[2]])
+  #   }
+  # }
+  # res<-dftmp
+  # #res<-summary(resamples(models[df$model]), metric='RMSE', decreasing=TRUE)
+  # rmse_3rd<-res[1,5] #3rd quantile of the best model's RMSE
+  # idx_ok<-which(res[,2]<rmse_3rd) #Which models are not statistically worse then the best
+  cat(paste0("Discarded ", nrow(df)-length(idx_ok), " models that are not as good as the best model\n"))
+  #  df<-dplyr::arrange(df[idx_ok,], rmse)
+  df<-dplyr::inner_join(df, tibble::tibble(model=idx_ok), by='model')
+  res<-caret::resamples(models[df$model])
+
+
+  return(list(df=df, resamples=res))
+}
+
--- batch.R
+++ batch.R
@@ -1,43 +1,5 @@
-
-# libs<-which_packages_are_missing(models_to_test)
-#
-# install.packages(libs)
-# summary(get_models_with_tags(type='Classification'))
-
-#models_to_test<-get_all_caret_models(type='Classification')
-#models_to_try<-which_models_are_ready_to_be_used(models_to_test)
-#dump('models_to_try', file = "")
-
-library(data.table)
-
-models_to_test<-get_all_caret_models(type='Classification')
-ans<-get_models_with_tags('Classification')
-
-models_to_try<-models_to_test[which(ans$`Tree-Based Model`)]
-
-colnames(ans)
-
-models_to_try <-
-  c("avNNet", "bag", "bagEarth", "bagEarthGCV", "bagFDAGCV", "bam",
-    "bayesglm", "blackboost", "BstLm", "bstSm", "bstTree", "C5.0",
-    "C5.0Cost", "C5.0Rules", "C5.0Tree", "cforest", "ctree", "ctree2",
-    "dnn", "earth", "evtree", "FH.GBML", "FRBCS.CHI", "FRBCS.W",
-    "gam", "gamboost", "gaussprLinear", "gaussprPoly", "gaussprRadial",
-    "gbm_h2o", "gbm", "gcvEarth", "glm", "glmboost", "glmnet_h2o",
-    "glmnet", "glmStepAIC", "J48", "JRip", "kernelpls", "kknn", "knn",
-    "lda", "lda2", "LMT", "loclda", "LogitBoost", "lssvmLinear",
-    "lssvmPoly", "lssvmRadial", "lvq", "mlp", "mlpML", "mlpWeightDecay",
-    "mlpWeightDecayML", "monmlp", "msaenet", "multinom", "nb", "nnet",
-    "nodeHarvest", "null", "OneR", "parRF", "PART", "partDSA", "pcaNNet",
-    "pls", "plsRglm", "polr", "qda", "ranger", "rbf", "rbfDDA", "rda",
-    "regLogistic", "rf", "rfRules", "rpart", "rpart1SE", "rpart2",
-    "rpartCost", "RRF", "RRFglobal", "simpls", "SLAVE", "slda", "spls",
-    "stepLDA", "stepQDA", "svmBoundrangeString", "svmExpoString",
-    "svmLinear", "svmLinear2", "svmLinear3", "svmLinearWeights",
-    "svmLinearWeights2", "svmPoly", "svmRadial", "svmRadialCost",
-    "svmRadialSigma", "svmRadialWeights", "svmSpectrumString", "treebag",
-    "widekernelpls", "xgbDART", "xgbLinear", "xgbTree", "xyf")
-
+library(doMC)
+registerDoMC(32)
 
 models_to_try <-
   c("ada", "AdaBag", "AdaBoost.M1", "adaboost", "avNNet", "awnb",
@@ -71,12 +33,7 @@ models_to_try <-
     "tanSearch", "treebag", "vglmAdjCat", "vglmContRatio", "vglmCumulative",
     "widekernelpls", "wsrf", "xgbDART", "xgbLinear", "xgbTree", "xyf"
   )
-
-models_to_try <-
-  c("cforest")
-
-model_names<-models_to_try
-
+library(flix)
 library(data.table)
 library(plyr)
 library(dplyr)
@@ -90,9 +47,10 @@ mydf<-load()
 dv_name<-'y'
 iv_names<-setdiff(colnames(mydf), 'y')
 
-calc_models(model_names, dv_nr=dv_nr, adaptive = NA, assume_calculated = TRUE)
 
 dt<-make_ads(dt = mydf, iv_names = iv_names, dv_name = dv_name, keep_nominal = FALSE)
 
-ans<-calc_models(model_names=models_to_try, cbind(dt, y=mydf[[dv_name]]), dv_name, iv_names, adaptive = TRUE, assume_calculated=TRUE)
+#models_to_try <- c("ada")
+#models_to_try <- c("ctree")
 
+ans<-calc_models(model_names=models_to_try, cbind(dt, y=mydf[[dv_name]]), dv_name, iv_names, adaptive = TRUE )
